0baaffb1eee16ea1a255d920551a3cdad2147203
Skrypchenko Mykhailo <skry@MacBook-Pro-Skrypchenko.local>
2020-05-17 13:16:57 +0300

    add localStorage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-'use strict'
+'use strict';
+
+let clients = JSON.parse(localStorage.getItem('clientsLS')) || []
+
+function saveData() {
+  localStorage.setItem('clientsLS', JSON.stringify(clients))
+}
 
 // Описываем класс создаваемого клиента
 class Client {
@@ -13,12 +19,30 @@ class Client {
 }
 
 // Находим необходимые для работы элементы
-let inputs = document.querySelectorAll('input')
+let inputs = document.querySelectorAll('.inputs input')
 let $createClientBtn = document.getElementById('createClientBtn')
 let $showFormBtn = document.getElementById('showFormBtn')
 let $list = document.querySelector('tbody')
 
-let clients = []
+renderList(clients)
+
+// Фильтрация
+let filterFields = document.querySelectorAll('.filter input')
+filterFields.forEach((input, i, arr) => {
+  input.addEventListener('dblclick', function() {
+    input.removeAttribute('readonly')
+  })
+  input.addEventListener('blur', function() {
+    input.setAttribute('readonly', 'readonly')
+  })
+  input.addEventListener('keyup', function() {
+    let filteredClients = clients.filter(client =>
+      client.name.toLowerCase().includes(arr[0].value.toLowerCase()) &&
+      client.age.includes(arr[1].value) &&
+      client.procedure.includes(arr[2].value))
+    renderList(filteredClients)
+  })
+})
 
 // Реализуем простую валидаюцию формы (1й инпут)
 inputs[0].addEventListener('keyup', function() {
@@ -41,23 +65,24 @@ function addClient() {
   let client = new Client(name, age, procedure)
   clients.push(client)
   renderList(clients)
+  saveData()
   inputs.forEach((elem) => elem.value = '')
   $createClientBtn.setAttribute('disabled', 'disabled')
 }
 
 // Добавление клиента в список в HTML
-function addToList(client) {
-  let $client = document.createElement('tr')
-  $client.innerHTML = `
-    <td>${client.name}</td>
-    <td>${client.age}</td>
-    <td>${client.procedure}</td>
-    <td><span class="delete">x</span></td>
-  `
-  let $delItemBtn = $client.querySelector('.delete')
-  $delItemBtn.onclick = deleteItem
-  $list.append($client)
-}
+// function addToList(client) {
+//   let $client = document.createElement('tr')
+//   $client.innerHTML = `
+//     <td>${client.name}</td>
+//     <td>${client.age}</td>
+//     <td>${client.procedure}</td>
+//     <td><span class="delete">x</span></td>
+//   `
+//   let $delItemBtn = $client.querySelector('.delete')
+//   $delItemBtn.onclick = deleteItem
+//   $list.append($client)
+// }
 
 function renderList(clients) {
   $list.innerHTML = ''
@@ -70,21 +95,24 @@ function renderList(clients) {
       <td><span class="delete">x</span></td>
     `
     let $delItemBtn = $client.querySelector('.delete')
-    $delItemBtn.onclick = deleteItem
+    $delItemBtn.onclick = function() {
+      deleteItem(client.id)
+    }
     $list.append($client)
   }
 }
 
+
 // Удаление клиента из списка
-function deleteItem() {
-  let el = this.parentElement.parentElement
-  // el.parentElement.removeChild(el)
-  el.remove()
-  console.log(clients)
+function deleteItem(id) {
+  clients = clients.filter(el => el.id != id)
+  renderList(clients)
+  saveData()
 }
 
 // Показать/скрыть форму
-$showFormBtn.onclick = function() {
+$showFormBtn.onclick = function(marina) {
+  console.log(marina)
   document.querySelector('.inputs').classList.toggle('inputs--full')
   this.innerText = this.innerText.includes('Показать') ? 'Скрыть' : 'Показать'
   this.innerText += ' поля'
